5866a068db70a3dae7d9cb0749c35c9833cf100c
KANNAN84-hash <kannanchainsys1234@gmail.com>
2021-06-03 05:47:39 +0530

    feature1 modified

--- sql/movies.sql
+++ sql/movies.sql
@@ -1,5 +1,5 @@
 create table movies(movie_id int not null , 
-				   movie_name varchar(255) not null,
+				   movie_name varchar(255) not null unique,
 				   release_date date not null,status boolean not null,
 					end_date date not null, primary key(movie_id)
 				   );

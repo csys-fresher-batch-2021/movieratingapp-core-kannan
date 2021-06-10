43faa859ba7cb144e7ef308e29e78de76a44423a
KANNAN84-hash <kannanchainsys1234@gmail.com>
2021-06-10 16:17:07 +0530

    added features3

---
+++ sql/insertmovies.sql
@@ -0,0 +1,6 @@
+insert into movies(movie_name,release_date) values ('Master','2021-01-13');
+insert into movies(movie_name,release_date) values ('Eswaran','2021-01-14');
+insert into movies(movie_name,release_date) values ('Sangathalaivan','2021-02-26');
+insert into movies(movie_name,release_date) values ('Teddy','2021-03-12');
+
+	
--- sql/movies.sql
+++ sql/movies.sql
@@ -1,9 +1,11 @@
 create table movies(movie_id  serial primary key ,movie_name varchar(255) not null unique,release_date date not null ,
 					status boolean default true  );
 					
-	insert into movies(movie_name,release_date) values ('Master','2020-12-01');
-    insert into movies(movie_name,release_date) values ('Karnan','2021-02-01');
-	insert into movies(movie_name,release_date) values ('Eswaran','2020-12-01');
+insert into movies(movie_name,release_date) values ('Master','2021-01-13');
+insert into movies(movie_name,release_date) values ('Eswaran','2021-01-14');
+insert into movies(movie_name,release_date) values ('Sangathalaivan','2021-02-26');
+insert into movies(movie_name,release_date) values ('Teddy','2021-03-12');
+
 	
 	
 create table users(id serial primary key,name varchar(255) not null ,
